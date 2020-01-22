5e685322159929dc02ee8abe6cecb7a4bee0bf16
renamoo <renamoo808@gmail.com>
2020-01-22 23:02:23 +0900

    refs #6 add auth ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
+import { firebaseConfig } from 'firebase.init';
+import * as firebase from 'firebase/app';
+import * as firebaseui from 'firebaseui';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +24,15 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+    firebase.initializeApp(firebaseConfig);
+    // firebase.analytics();
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Other config options...
     });
   }
 }
